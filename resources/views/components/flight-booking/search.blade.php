274c5dd58a76b2afcb32bba639c7a003eea80c88
Alaa M Shaban <ala96ala96@gmail.com>
2023-03-27 07:02:06 +0200

    import tempalte

---
+++ resources/views/components/flight-booking/search.blade.php
@@ -0,0 +1,260 @@
+<div class="search-area">
+    <div class="container">
+        <div class="search-wrapper">
+            <!-- flight search -->
+            <div class="search-box flight-search">
+                <div class="search-form">
+                    <form action="#">
+                        <!-- flight type -->
+                        <div class="flight-type">
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" checked value="one-way"
+                                    name="flight-type" id="flight-type1">
+                                <label class="form-check-label" for="flight-type1">
+                                    One Way
+                                </label>
+                            </div>
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" value="round-way"
+                                    name="flight-type" id="flight-type2">
+                                <label class="form-check-label" for="flight-type2">
+                                    Round Way
+                                </label>
+                            </div>
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" value="multi-city"
+                                    name="flight-type" id="flight-type3">
+                                <label class="form-check-label" for="flight-type3">
+                                    Multi City
+                                </label>
+                            </div>
+                        </div>
+                        <!-- flight type end -->
+
+                        <!-- flight search wrapper -->
+                        <div class="flight-search-wrapper">
+                            <div class="flight-search-content">
+                                <!-- flight search content -->
+                                <div class="flight-search-item">
+                                    <div class="row">
+                                        <div class="col-lg-3">
+                                            <div class="form-group">
+                                                <label>From</label>
+                                                <div class="form-group-icon">
+                                                    <input type="text" name="from-destination"
+                                                        class="form-control swap-from" value="New York">
+                                                    <i class="fal fa-plane-departure"></i>
+                                                </div>
+                                                <p>JFK - John F. Kennedy International Airport
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-3">
+                                            <div class="form-group">
+                                                <div class="search-form-swap"><i class="far fa-repeat"></i>
+                                                </div>
+                                                <label>To</label>
+                                                <div class="form-group-icon">
+                                                    <input type="text" name="to-destination"
+                                                        class="form-control swap-to" value="Los Angeles">
+                                                    <i class="fal fa-plane-arrival"></i>
+                                                </div>
+                                                <p>LAX - Los Angeles International Airport</p>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-3">
+                                            <div class="form-group">
+                                                <div class="search-form-date">
+                                                    <div class="search-form-journey">
+                                                        <label>Journey Date</label>
+                                                        <div class="form-group-icon">
+                                                            <input type="text" name="journey-date"
+                                                                class="form-control date-picker journey-date">
+                                                            <i class="fal fa-calendar-days"></i>
+                                                        </div>
+                                                        <p class="journey-day-name"></p>
+                                                    </div>
+                                                    <div class="search-form-return">
+                                                        <label>Return Date</label>
+                                                        <div class="form-group-icon">
+                                                            <input type="text" name="return-date"
+                                                                class="form-control date-picker return-date">
+                                                        </div>
+                                                        <p class="return-day-name"></p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-3">
+                                            <div class="form-group dropdown static-dropdown passenger-box">
+                                                <div class="passenger-class" role="menu" data-bs-toggle="dropdown"
+                                                    data-bs-display="static" aria-expanded="false">
+                                                    <label>Passenger, Class</label>
+                                                    <div class="form-group-icon">
+                                                        <div class="passenger-total"><span
+                                                                class="passenger-total-amount">2</span>
+                                                            Passenger
+                                                        </div>
+                                                        <i class="fal fa-user-plus"></i>
+                                                    </div>
+                                                    <p class="passenger-class-name">Business</p>
+                                                </div>
+                                                <div class="dropdown-menu dropdown-menu-end">
+                                                    <div class="dropdown-item">
+                                                        <div class="passenger-item">
+                                                            <div class="passenger-info">
+                                                                <h6>Adults</h6>
+                                                                <p>12+ Years</p>
+                                                            </div>
+                                                            <div class="passenger-qty">
+                                                                <button type="button" class="minus-btn"><i
+                                                                        class="far fa-minus"></i></button>
+                                                                <input type="text" name="adult"
+                                                                    class="qty-amount passenger-adult" value="2"
+                                                                    readonly>
+                                                                <button type="button" class="plus-btn"><i
+                                                                        class="far fa-plus"></i></button>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="dropdown-item">
+                                                        <div class="passenger-item">
+                                                            <div class="passenger-info">
+                                                                <h6>Children</h6>
+                                                                <p>2-12 Years</p>
+                                                            </div>
+                                                            <div class="passenger-qty">
+                                                                <button type="button" class="minus-btn"><i
+                                                                        class="far fa-minus"></i></button>
+                                                                <input type="text" name="children"
+                                                                    class="qty-amount passenger-children"
+                                                                    value="0" readonly>
+                                                                <button type="button" class="plus-btn"><i
+                                                                        class="far fa-plus"></i></button>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="dropdown-item">
+                                                        <div class="passenger-item">
+                                                            <div class="passenger-info">
+                                                                <h6>Infant</h6>
+                                                                <p>Below 2 Years</p>
+                                                            </div>
+                                                            <div class="passenger-qty">
+                                                                <button type="button" class="minus-btn"><i
+                                                                        class="far fa-minus"></i></button>
+                                                                <input type="text" name="infant"
+                                                                    class="qty-amount passenger-infant"
+                                                                    value="0" readonly>
+                                                                <button type="button" class="plus-btn"><i
+                                                                        class="far fa-plus"></i></button>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="dropdown-item">
+                                                        <h6 class="mb-3 mt-2">Cabin Class</h6>
+                                                        <div class="passenger-class-info">
+                                                            <div class="form-check">
+                                                                <input class="form-check-input" type="radio"
+                                                                    value="Economy" name="cabin-class"
+                                                                    id="cabin-class1">
+                                                                <label class="form-check-label"
+                                                                    for="cabin-class1">
+                                                                    Economy
+                                                                </label>
+                                                            </div>
+                                                            <div class="form-check">
+                                                                <input class="form-check-input" checked
+                                                                    type="radio" value="Business"
+                                                                    name="cabin-class" id="cabin-class2">
+                                                                <label class="form-check-label"
+                                                                    for="cabin-class2">
+                                                                    Business
+                                                                </label>
+                                                            </div>
+                                                            <div class="form-check">
+                                                                <input class="form-check-input" type="radio"
+                                                                    value="First Class" name="cabin-class"
+                                                                    id="cabin-class3">
+                                                                <label class="form-check-label"
+                                                                    for="cabin-class3">
+                                                                    First Class
+                                                                </label>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- flight search content end -->
+
+                                <!-- flight-multicity-item -->
+                                <div class="flight-search-item flight-multicity-item have-to-clone">
+                                    <div class="row">
+                                        <div class="col-lg-3">
+                                            <div class="form-group">
+                                                <label>From</label>
+                                                <div class="form-group-icon">
+                                                    <input type="text" name="from-destination"
+                                                        class="form-control swap-from" value="New York">
+                                                    <i class="fal fa-plane-departure"></i>
+                                                </div>
+                                                <p>JFK - John F. Kennedy International Airport
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-3">
+                                            <div class="form-group">
+                                                <div class="search-form-swap"><i class="far fa-repeat"></i>
+                                                </div>
+                                                <label>To</label>
+                                                <div class="form-group-icon">
+                                                    <input type="text" name="to-destination"
+                                                        class="form-control swap-to" value="Los Angeles">
+                                                    <i class="fal fa-plane-arrival"></i>
+                                                </div>
+                                                <p>LAX - Los Angeles International Airport</p>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-3">
+                                            <div class="form-group">
+                                                <div class="search-form-date">
+                                                    <div class="search-form-journey">
+                                                        <label>Journey Date</label>
+                                                        <div class="form-group-icon">
+                                                            <input type="text" name="journey-date"
+                                                                class="form-control date-picker journey-date">
+                                                            <i class="fal fa-calendar-days"></i>
+                                                        </div>
+                                                        <p class="journey-day-name"></p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-3">
+                                            <div class="form-group">
+                                                <div class="multicity-btn">
+                                                    <i class="fal fa-plus-circle"></i> Add
+                                                    Another Flight
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- flight multicity end -->
+                            </div>
+                            <div class="search-btn">
+                                <button type="submit" class="theme-btn"><span
+                                        class="far fa-search"></span>Update Search</button>
+                            </div>
+                        </div>
+                        <!-- flight search wrapper end -->
+                    </form>
+                </div>
+            </div>
+            <!-- flight search end -->
+        </div>
+    </div>
+</div>
---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- meta tags -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+
+    <!-- title -->
+    <title>MyTrip </title>
+
+    <!-- favicon -->
+    <link rel="icon" type="image/x-icon" href="{{ asset("assets/img/logo/favicon.png") }}">
+
+    @include("layout.incloude.style")
+    @yield("style")
+
+</head>
+
+<body>
+
+    <!-- preloader -->
+    <div class="preloader">
+        <div class="loader">
+            <span style="--i:1;"></span>
+            <span style="--i:2;"></span>
+            <span style="--i:3;"></span>
+            <span style="--i:4;"></span>
+            <span style="--i:5;"></span>
+            <span style="--i:6;"></span>
+            <span style="--i:7;"></span>
+            <span style="--i:8;"></span>
+            <span style="--i:9;"></span>
+            <span style="--i:10;"></span>
+            <span style="--i:11;"></span>
+            <span style="--i:12;"></span>
+            <span style="--i:13;"></span>
+            <span style="--i:14;"></span>
+            <span style="--i:15;"></span>
+            <span style="--i:16;"></span>
+            <span style="--i:17;"></span>
+            <span style="--i:18;"></span>
+            <span style="--i:19;"></span>
+            <span style="--i:20;"></span>
+            <div class="loader-plane"></div>
+        </div>
+    </div>
+    <!-- preloader end -->
+
+
+    <!-- header area -->
+    @include("layout.header")
+    <!-- header area end -->
+
+
+
+
+    <main class="main">
+
+       @yield("content")
+
+    </main>
+
+
+
+    <!-- footer area -->
+    @include("layout.footer")
+    <!-- footer area end -->
+
+
+
+
+    <!-- scroll-top -->
+    <a href="#" id="scroll-top"><i class="far fa-angle-up"></i></a>
+    <!-- scroll-top end -->
+
+
+    @include("layout.incloude.script")
+    @yield("script")
+
+</body>
+
+</html>
