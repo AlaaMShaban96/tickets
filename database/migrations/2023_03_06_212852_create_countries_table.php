448538d26b0e444ba376605bd3ed2230887621a9
AlaaMShaban <ala96ala96@gmail.com>
2023-04-06 09:17:50 +0200

    edit size of column name in table country

--- database/migrations/2023_03_06_212852_create_countries_table.php
+++ database/migrations/2023_03_06_212852_create_countries_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('countries', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 20);
+            $table->string('name', 70);
             $table->timestamp('deleted_at')->nullable();
             $table->timestamps();
         });
